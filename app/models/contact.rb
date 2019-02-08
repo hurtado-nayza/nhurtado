9297f33b3b5512dc9f5d115940b304e3e37ad0ee
Nayza <hurtado.nayza@gmail.com>
2019-02-08 15:41:48 -0400

    internalizacion y arreglo del footer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
+	resourcify
 	validates :name, :email, :phone, :message, presence: true
 	validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Formato incorrecto" }
 	validates :phone, format: {with: /\+(\d{1,3})\s+(\d{3})\s+(\d{7})/, message: "Ej: +58 414 8076973"}
